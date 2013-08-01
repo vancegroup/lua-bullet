fa714922214f111a44fe69d84b8847c06a755182
Patrick Carlson <carlsonp@iastate.edu>
2013-08-01 14:02:01 -0500

    add binding for global enum (is this right?)

--- src/BulletCollision/BroadphaseCollision/btBroadphaseProxy.cpp
+++ src/BulletCollision/BroadphaseCollision/btBroadphaseProxy.cpp
@@ -39,9 +39,49 @@ template<> luabind::scope getLuaBinding<btBroadphaseProxy>() {
 	    .def(constructor<>())
 	    .def(constructor<const btVector3 &, const btVector3 &, void *, short int, short int, void *>())
 	    .def("getUid", &btBroadphaseProxy::getUid)
-	    ///@TODO finish binding the rest of these properties if they're needed
+	    ///@TODO finish binding the rest of the properties if they're needed
 	    .property("m_uniqueId", &btBroadphaseProxy::m_uniqueId)
 	    .property("m_aabbMin", &btBroadphaseProxy::m_aabbMin)
 	    .property("m_aabbMax", &btBroadphaseProxy::m_aabbMax)
+	    //http://www.rasterbar.com/products/luabind/docs.html#enums
+	    //add binding for various enums
+	    ///@TODO is this the correct way to bind these enums since they're global?
+	    .enum_("BroadphaseNativeTypes")
+	    [
+            value("BOX_SHAPE_PROXYTYPE", BOX_SHAPE_PROXYTYPE),
+            value("TRIANGLE_SHAPE_PROXYTYPE", TRIANGLE_SHAPE_PROXYTYPE),
+            value("TETRAHEDRAL_SHAPE_PROXYTYPE", TETRAHEDRAL_SHAPE_PROXYTYPE),
+            value("CONVEX_TRIANGLEMESH_SHAPE_PROXYTYPE", CONVEX_TRIANGLEMESH_SHAPE_PROXYTYPE),
+            value("CONVEX_HULL_SHAPE_PROXYTYPE", CONVEX_HULL_SHAPE_PROXYTYPE),
+            value("CONVEX_POINT_CLOUD_SHAPE_PROXYTYPE", CONVEX_POINT_CLOUD_SHAPE_PROXYTYPE),
+            value("CUSTOM_POLYHEDRAL_SHAPE_TYPE", CUSTOM_POLYHEDRAL_SHAPE_TYPE),
+            value("SPHERE_SHAPE_PROXYTYPE", SPHERE_SHAPE_PROXYTYPE),
+            value("MULTI_SPHERE_SHAPE_PROXYTYPE", MULTI_SPHERE_SHAPE_PROXYTYPE),
+            value("CAPSULE_SHAPE_PROXYTYPE", CAPSULE_SHAPE_PROXYTYPE),
+            value("CONE_SHAPE_PROXYTYPE", CONE_SHAPE_PROXYTYPE),
+            value("CONVEX_SHAPE_PROXYTYPE", CONVEX_SHAPE_PROXYTYPE),
+            value("CYLINDER_SHAPE_PROXYTYPE", CYLINDER_SHAPE_PROXYTYPE),
+            value("UNIFORM_SCALING_SHAPE_PROXYTYPE", UNIFORM_SCALING_SHAPE_PROXYTYPE),
+            value("MINKOWSKI_SUM_SHAPE_PROXYTYPE", MINKOWSKI_SUM_SHAPE_PROXYTYPE),
+            value("MINKOWSKI_DIFFERENCE_SHAPE_PROXYTYPE", MINKOWSKI_DIFFERENCE_SHAPE_PROXYTYPE),
+            value("BOX_2D_SHAPE_PROXYTYPE", BOX_2D_SHAPE_PROXYTYPE),
+            value("CONVEX_2D_SHAPE_PROXYTYPE", CONVEX_2D_SHAPE_PROXYTYPE),
+            value("CUSTOM_CONVEX_SHAPE_TYPE", CUSTOM_CONVEX_SHAPE_TYPE),
+            value("TRIANGLE_MESH_SHAPE_PROXYTYPE", TRIANGLE_MESH_SHAPE_PROXYTYPE),
+            value("SCALED_TRIANGLE_MESH_SHAPE_PROXYTYPE", SCALED_TRIANGLE_MESH_SHAPE_PROXYTYPE),
+            value("FAST_CONCAVE_MESH_PROXYTYPE", FAST_CONCAVE_MESH_PROXYTYPE),
+            value("TERRAIN_SHAPE_PROXYTYPE", TERRAIN_SHAPE_PROXYTYPE),
+            value("GIMPACT_SHAPE_PROXYTYPE", GIMPACT_SHAPE_PROXYTYPE),
+            value("MULTIMATERIAL_TRIANGLE_MESH_PROXYTYPE", MULTIMATERIAL_TRIANGLE_MESH_PROXYTYPE),
+            value("EMPTY_SHAPE_PROXYTYPE", EMPTY_SHAPE_PROXYTYPE),
+            value("STATIC_PLANE_PROXYTYPE", STATIC_PLANE_PROXYTYPE),
+            value("CUSTOM_CONCAVE_SHAPE_TYPE", CUSTOM_CONCAVE_SHAPE_TYPE),
+            value("COMPOUND_SHAPE_PROXYTYPE", COMPOUND_SHAPE_PROXYTYPE),
+            value("SOFTBODY_SHAPE_PROXYTYPE", SOFTBODY_SHAPE_PROXYTYPE),
+            value("HFFLUID_SHAPE_PROXYTYPE", HFFLUID_SHAPE_PROXYTYPE),
+            value("HFFLUID_BUOYANT_CONVEX_SHAPE_PROXYTYPE", HFFLUID_BUOYANT_CONVEX_SHAPE_PROXYTYPE),
+            value("INVALID_SHAPE_PROXYTYPE", INVALID_SHAPE_PROXYTYPE),
+            value("MAX_BROADPHASE_COLLISION_TYPES", MAX_BROADPHASE_COLLISION_TYPES)
+	    ]
 	    ;
 }
