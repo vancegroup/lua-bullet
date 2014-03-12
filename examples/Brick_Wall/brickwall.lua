88ad5c62a79d96ae1b9a769959f4cadf3c4ed20c
Patrick Carlson <carlsonp@iastate.edu>
2014-03-12 15:35:30 -0500

    cleanup and get example working

--- examples/Brick_Wall/brickwall.lua
+++ examples/Brick_Wall/brickwall.lua
@@ -3,6 +3,7 @@ require "AddAppDirectory"
 AddAppDirectory()
 runfile[[../simpleLights.lua]]
 runfile[[../bitwise_inclusive_or.lua]]
+
 local device = gadget.PositionInterface("VJWand")
 
 local function initPhysics()
@@ -10,11 +11,14 @@ local function initPhysics()
 	collisionConfiguration = bullet.btDefaultCollisionConfiguration(constructionInfo)
 	dispatcher = bullet.btCollisionDispatcher(collisionConfiguration)
 	solver = bullet.btSequentialImpulseConstraintSolver()
-	worldAabbMin = bullet.btVector3(-10000, -10000, -10000)
-	worldAabbMax = bullet.btVector3(10000, 10000, 10000)
-	inter = bullet.btAxisSweep3(worldAabbMin, worldAabbMax, 1000)
-	dynamicsWorld = bullet.btDiscreteDynamicsWorld(dispatcher, inter, solver, collisionConfiguration)
-	dynamicsWorld:setGravity(bullet.btVector3(0,-9.8, 0))
+	--Declare broadphase collision algorithm (btDbvtBroadphase)
+	-- other algorithms include: bt32BitAxisSweep3, btAxisSweep3
+	broadphase = bullet.btDbvtBroadphase()
+	dynamicsWorld = bullet.btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration)
+	--set gravity (currently -9.8 meters in the Y direction)
+	dynamicsWorld:setGravity(bullet.btVector3(0, -9.8, 0))
+	--Not needed, but changes all collisions with a gimpact trimesh to be solved with gimpact.
+	--bullet.btGImpactCollisionAlgorithm.registerAlgorithm(dispatcher)
 	return dynamicsWorld
 end
 
@@ -30,7 +34,6 @@ function createBrickWall()
 	for j=1, bricks_high do
 		-- Stagger odd rows
 		if j%2 == 0 then pos_x = 0 else pos_x = brickLength/3 end
-
 		-- For each brick in a row
 		for i=1, bricks_wide do
 			local newBrick = MatrixTransform{
@@ -39,13 +42,15 @@ function createBrickWall()
 					brick,
 				}
 			}
-
 			local brickRigidBody = osgbDynamics.createRigidBody{
+				--Declare shape type of object (box primitive)
+				-- others include: TRIANGLE_MESH_SHAPE_PROXYTYPE,CONVEX_HULL_SHAPE_PROXYTYPE, GIMPACT_SHAPE_PROXYTYPE
 				shapeType = bullet.btBroadphaseProxy.BOX_SHAPE_PROXYTYPE,
 				sceneGraph = newBrick,
-				mass = 10.0,
-				restitution = 1
+				mass = 1.0,
+				restitution = 0.5
 			}
+			--add brickRigidBody to bulletWorld
 			bulletWorld:addRigidBody(brickRigidBody)
 			brickWall:addChild(newBrick)
 			pos_x = pos_x + (brickLength*.90)
@@ -65,71 +70,66 @@ local function createFloor()
 	local floorRigidBody = osgbDynamics.createRigidBody{
 		sceneGraph = floorObj,
 		mass = 0.0,
-		restitution = 0.0
+		restitution = 0.5
 	}
 	bulletWorld:addRigidBody(floorRigidBody)
 	RelativeTo.World:addChild(floorObj)
 end
 
-local function createKinematicTeapot(bw, startingPos)
+local function createKinematicTeapot(startingPos)
 	local root = MatrixTransform{
 		Transform{
 			position = startingPos,
-			--scale = 0.3,
-			--Model("../../assets/teapot.osg")
-			Sphere{
-				radius = .125
-			}
+			scale = 0.3,
+			Model("../../assets/teapot.osg")
 		}
 	}
 	-- this differs slightly from the C++ (see RigidBody.cpp for details on what can be passed in)
 	local body = osgbDynamics.createRigidBody{
 		sceneGraph = root,
-		--shapeType = bullet.btBroadphaseProxy.TRIANGLE_MESH_SHAPE_PROXYTYPE,
-		-- This should be used for concave shapes? (like a teapot)
-		--shapeType = bullet.btBroadphaseProxy.GIMPACT_SHAPE_PROXYTYPE,
-		-- This should be used for a convex hull shapes (like a sphere)
-		--shapeType = bullet.btBroadphaseProxy.CONVEX_HULL_SHAPE_PROXYTYPE,
+		-- Declare shape type for non primitive objects - use GIMPACT_SHAPE_PROXYTYPE (like our teapot)
+		shapeType = bullet.btBroadphaseProxy.GIMPACT_SHAPE_PROXYTYPE,
 		mass = 1.0,
 		restitution = 1.0
 	}
-	--not sure about parameters here: see http://www.bulletphysics.org/Bullet/phpBB3/viewtopic.php?f=9&t=7195
+	--set KINEMATIC flag for body (required if you want to move the body manually)
 	body:setCollisionFlags(bitor(body:getCollisionFlags(), bullet.btCollisionObject.CF_KINEMATIC_OBJECT))
+	-- disable deactivation for body (we always want to check for collisions for the body)
 	body:setActivationState(bullet.btCollisionObject.DISABLE_DEACTIVATION)
-	bw:addRigidBody(body)
+	--add body to bulletWorld
+	bulletWorld:addRigidBody(body)
 	return root, body
 end
 
-local function setKinematicBodyAtWand(body)
-	--get body position
-	local xform = bullet.btTransform()
-	body:getMotionState():getWorldTransform(xform)
-	local origin = xform:getOrigin()
-	--set new pos based on last pos
-	local wandDelta = device.position - lastWandPos
-	local newOrigin = bullet.btVector3(wandDelta:x()+origin:getX(), wandDelta:y()+origin:getY(), wandDelta:z()+origin:getZ())
-	xform:setOrigin(newOrigin)
-	body:getMotionState():setWorldTransform(xform)
-	lastWandPos = device.position
-end
-
-lastWandPos = device.position
 --create a "bulletWorld" main object to handle bullet physics & objects
 bulletWorld = initPhysics()
 createFloor()
 createBrickWall()
-teapot_osg, teapotRigidBody = createKinematicTeapot(bulletWorld, device.position)
+teapot_osg, teapotRigidBody = createKinematicTeapot(device.position)
 --add teapot_osg to world
 RelativeTo.World:addChild(teapot_osg)
 
---add a frame action to step the simulation based on dt
+--REQUIRED: add a frame action to step the simulation based on dt
 Actions.addFrameAction(function()
-	-- wait 3 seconds before starting simulation
-	Actions.waitSeconds(3)
 	while true do
-		setKinematicBodyAtWand(teapotRigidBody)
 		elapsed_time = Actions.waitForRedraw()
 		bulletWorld:stepSimulation(elapsed_time)
+	end
+end)
+
+--OPTIONAL: add a frame action to set teapot position to wand position (for interaction)
+Actions.addFrameAction(function()
+	while true do
+		local xform = bullet.btTransform()
+		local rotMatrixOfWand = device.matrix:getRotate()
+		local quat = bullet.btQuaternion(rotMatrixOfWand:x(),rotMatrixOfWand:y(),rotMatrixOfWand:z(),rotMatrixOfWand:w())
+		teapotRigidBody:getMotionState():getWorldTransform(xform)
+		local origin = xform:getOrigin()
+		local newOrigin = bullet.btVector3(device.position:x(), device.position:y(), device.position:z())
+		xform:setOrigin(newOrigin)
+		xform:setRotation(quat)
+		teapotRigidBody:getMotionState():setWorldTransform(xform)
+		Actions.waitForRedraw()
 	end
 end)
 
